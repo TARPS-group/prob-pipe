087e3f5503da943e2ebf93b85e15bbd945b8e730
Can Erozer <caner@bu.edu>
2025-11-19 15:44:41 +0300

    new structuring for CI tests. Andrew should now be able to pass the CI layer.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from probpipe.core.distributions import EmpiricalDistribution
+from probpipe import EmpiricalDistribution
 
 @pytest.fixture
 def rng():
@@ -17,3 +17,17 @@ def empirical(simple_samples, rng):
 @pytest.fixture
 def simple_weights():
     return np.array([0.2, 0.3, 0.5])
+
+@pytest.fixture
+def dim():
+    return 3
+
+@pytest.fixture
+def mean(dim):
+    return np.arange(dim, dtype=float)  # [0,1,2]
+
+@pytest.fixture
+def cov_matrix(dim):
+    A = np.eye(dim) * 2.0
+    A[0,1] = A[1,0] = 0.3
+    return A
