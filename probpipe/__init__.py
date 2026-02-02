39bb5397fb6cbcc9b8f161fe24555c91bc934eb4
Can Erozer <caner@bu.edu>
2026-02-03 00:08:19 +0300

    changing the examples so that they are compatible with andrew's distributions

--- probpipe/__init__.py
+++ probpipe/__init__.py
@@ -1,35 +1,39 @@
+from probpipe.distributions.distribution import *
+from probpipe.distributions.real_vector.gaussian import *
+from probpipe.core.node import *
+from probpipe.core.mcmc import *
 
 # EmpiricalDistribution 
-try:
-    from probpipe.core.distributions import EmpiricalDistribution
-except Exception:
-    try:
-        from probpipe.distributions.distribution import EmpiricalDistribution
-    except Exception:
-        EmpiricalDistribution = None
+# try:
+#     from probpipe.core.distributions import EmpiricalDistribution
+# except Exception:
+#     try:
+#         from probpipe.distributions.distribution import EmpiricalDistribution
+#     except Exception:
+#         EmpiricalDistribution = None
 
 
-# Normal1D 
-try:
-    from probpipe.core.multivariate import Normal1D
-except Exception:
-    try:
-        from probpipe.distributions.multivariate import Normal1D
-    except Exception:
-        Normal1D = None
+# # Normal1D 
+# try:
+#     from probpipe.core.multivariate import Normal1D
+# except Exception:
+#     try:
+#         from probpipe.distributions.multivariate import Normal1D
+#     except Exception:
+#         Normal1D = None
 
 
-# MvNormal
-try:
-    from probpipe.core.multivariate import MvNormal
-except Exception:
-    try:
-        from probpipe.distributions.real_vector.gaussian import Gaussian
-    except Exception:
-        Gaussian = None
+# # MvNormal
+# try:
+#     from probpipe.core.multivariate import MvNormal
+# except Exception:
+#     try:
+#         from probpipe.distributions.real_vector.gaussian import Gaussian
+#     except Exception:
+#         Gaussian = None
 
-# Gaussian 
-try:
-    from probpipe.distributions.real_vector.gaussian import Gaussian
-except Exception:
-    Gaussian = None
+# # Gaussian 
+# try:
+#     from probpipe.distributions.real_vector.gaussian import Gaussian
+# except Exception:
+#     Gaussian = None
